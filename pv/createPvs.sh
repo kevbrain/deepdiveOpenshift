04ec87c81b7e6625ac152cb0fd0c91a146b406f5
root <root@bastion.ocp4.its4u.local>
2020-04-24 14:53:20 +0200

    Script demo DeepOpenshift

---
+++ pv/createPvs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+oc create -f pvrwm_001.yaml
+oc create -f pvrwm_002.yaml
+oc create -f pvrwm_003.yaml
+oc create -f pvrwo_001.yaml
+oc create -f pvrwo_002.yaml
+oc create -f pvrwo_003.yaml
+
